ba7823e48302b6ab1cba76f9cab4a05ce4ddf6c9
jashandeep <jashandeep1659@gmail.com>
2024-11-29 11:45:18 +0530

    feat: login and signup functions include prisma setup

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,15 +1,28 @@
 import { Button } from "@/components/ui/button";
+import { auth, signIn } from "@/lib/auth";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  const session = await auth();
+  if (session?.user) {
+    redirect("/");
+  }
   return (
     <div className="min-h-screen flex items-center justify-center bg-slate-100">
       <div className="p-4 border rounded-md bg-background min-w-[25%]">
         <h1 className="md:text-2xl font-bold text-center">Login</h1>
         <div className="mt-6">
-          <Button variant={"outline"} className="w-full">
-            Login with Google
-          </Button>
+          <form
+            action={async () => {
+              "use server";
+              await signIn("google");
+            }}
+          >
+            <Button type="submit" variant={"outline"} className="w-full">
+              Login with Google
+            </Button>
+          </form>
         </div>
       </div>
     </div>
